a1ac2c4bae1d8638c6e148bbccef1f24eb1627af
ray_yokoyama <kingreibit@gmail.com>
2020-12-18 08:15:05 +0900

    add 04

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -1,14 +1,61 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
+(INPUT)
+	@KBD
+	D=M
+	@FILL_WHITE
+	D;JEQ 
+	@FILL_BLACK
+	0;JMP
+@INPUT
+0;JMP
+(FILL_WHITE)
+    // const SCREEN = 16384
+    // let count = count
+    // cont color = white
+    // loop
+    //  count++
+    //  if (count + (512*256) - count === 0) end;
+    //  arr[count] = white
+    // end
+    @color
+	M=0
+	@SCREEN
+	D=A
+	@count
+	M=D // count = SCREEN
+  @FILL_LOOP
+  0;JMP
+(FILL_BLACK)
+    // const count = 16384
+    // let count = count
+    // cont color = black
+    // loop
+    //  count++
+    //  if (count + (512*256) - count === 0) end;
+    //  arr[count] = black
+    // end
+  @3
+  D=A
+  @color
+  M=D
+	@SCREEN
+	D=A
+	@count
+	M=D // count = SCREEN
+  @FILL_LOOP
+  0;JMP
+(FILL_LOOP)
+  @color 
+  D=M
+  @count
+  A=M
+  M=D // MEM[count] = color
+  @count
+  M=M+1 // screnn += 1
+  @24576 // SCREEN + (512*256) // SCREEN = 16384
+  D=A
+  @count
+  D=D-M // D = 24576 - count
+@FILL_LOOP
+D;JGT // if D > 0 goto FILL_LOOP
+@INPUT
+0;JMP
--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+ @i    // A = i → 111 
+ M=1   // i = 1, M[111] = 1
+ @sum
+ M=0   // sum = 0, M[sum] = 0
+(LOOP)
+ @i    // A = i
+ D=M   // D = i, D = M[A]
+ @100  // A = 100
+ D=D-A // D = i - 100
+ @END
+ D;JGT
+ @i
+ D=M
+ @sum
+ M=D+M // sum = i + sum
+ @i
+ M=M+1 // i ++
+ @LOOP
+ 0;JMP
+(END)
+ @END
+ 0;JMP
